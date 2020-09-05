117abd34e9afef1a7946602299bebcdc8b6bb425
DaanA32 <daan32@gmail.com>
2020-09-05 09:05:42 +0100

    update example

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,23 @@
 //! # Examples
 //!
 //! ```
-//! use mesh_generation::mesh::PolyMesh;
-//! use noise::{Perlin, Seedable};
+//! use mesh_generation:: {
+//!     generate_noise_map,
+//!     meshes::{PolyMesh},
+//!     noise::{Perlin, Seedable},
+//! };
 //! 
 //! let perlin = Perlin::new();
 //! perlin.set_seed(1564863213);
-//! let mut mesh = PolyMesh::new(Some(128), Some(128), Some(10), Some(10));
+//! let perlin_boxed = Box::new(perlin);
 //! let width = 128;
 //! let height = 128;
-//! let noise_map = mesh::generate_noise_map(perlin, width, height, 128.0, 5);
+//! let mut mesh = PolyMesh::new(width, height, 10, 10);
+//! let noise_map = generate_noise_map(perlin_boxed, width, height, 128.0, 5);
 //! mesh.displace_with_noise_map(noise_map, width, height);
 //! mesh.calculate_normals();
 //! mesh.export_to_obj("./poly_mesh.obj");
+//!
 //! ```
 
 pub mod meshes;
